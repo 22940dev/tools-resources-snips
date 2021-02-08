40d8e5c22a0235d0952719126360849ed69e31d1
Nikita Voloboev <nikita.voloboev@gmail.com>
2021-02-08 18:09:28 +0100

    .

--- docs/assets/js/just-the-docs.js
+++ docs/assets/js/just-the-docs.js
@@ -62,7 +62,7 @@
     }
   
     function initNavBar() {
-      return fetch("https://epictools.dev/nav.html").then(function (response) {
+      return fetch("http://localhost:8080/nav.html").then(function (response) {
         return response.text().then((content) => {
           replaceNav(content);
           return;
@@ -80,7 +80,7 @@
     function initSearch() {
       var request = new XMLHttpRequest();
       // TODO: CHECK IF THIS WORKS WITHOUT ABSOLUTE_URL
-      request.open("GET", "https://epictools.dev/assets/js/search-data.json", true);
+      request.open("GET", "http://localhost:8080/assets/js/search-data.json", true);
   
       request.onload = function () {
         if (request.status >= 200 && request.status < 400) {
@@ -514,7 +514,7 @@
       var cssFile = document.querySelector('[rel="stylesheet"]');
       cssFile.setAttribute(
         "href",
-        'https://epictools.dev/assets/css/just-the-docs-' + theme + ".css"
+        'http://localhost:8080/assets/css/just-the-docs-' + theme + ".css"
       );
     };
   
